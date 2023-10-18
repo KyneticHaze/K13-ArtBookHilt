af11a4d7daa6c8cb4a733d76563e930163d5c06b
furkan harmancı <rocklee34@hotmail.com>
2023-10-19 00:09:08 +0300

    test: liveData and hilt test files are added :rotating-light:

---
+++ app/src/test/java/com/example/artbooktestinghilt/repo/FakeArtRepository.kt
@@ -0,0 +1,36 @@
+package com.example.artbooktestinghilt.repo
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.artbooktestinghilt.model.ImageResponse
+import com.example.artbooktestinghilt.repository.ArtRepositoryInterface
+import com.example.artbooktestinghilt.roomdb.Art
+import com.example.artbooktestinghilt.util.Resource
+
+class FakeArtRepository: ArtRepositoryInterface {
+
+    private val arts = mutableListOf<Art>() // Art objesi listesi
+    private val artsLiveData = MutableLiveData<List<Art>>(arts) // Bu obje listesinin değiştirilebilir canlı veri tutması
+
+    override suspend fun insertArt(art: Art) {
+        arts.add(art)
+        refreshData()
+    }
+
+    override suspend fun deleteArt(art: Art) {
+        arts.remove(art)
+        refreshData()
+    }
+
+    override fun getArt(): LiveData<List<Art>> {
+        return artsLiveData
+    }
+
+    override suspend fun searchImage(imageString: String): Resource<ImageResponse> {
+        return Resource.success(ImageResponse(listOf(),0,0))
+    }
+
+    private fun refreshData() {
+        artsLiveData.postValue(arts)
+    }
+}
