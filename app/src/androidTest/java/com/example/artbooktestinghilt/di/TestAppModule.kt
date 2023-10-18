78b96fe87fabcbd2a6b0fdd1544c717aca647fe6
furkan harmancı <rocklee34@hotmail.com>
2023-10-19 00:10:27 +0300

    feat: hilt module object is added :sparkles:

---
+++ app/src/androidTest/java/com/example/artbooktestinghilt/di/TestAppModule.kt
@@ -0,0 +1,23 @@
+package com.example.artbooktestinghilt.di
+
+import android.content.Context
+import androidx.room.Room
+import com.example.artbooktestinghilt.roomdb.ArtDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Named
+
+@Module
+@InstallIn(SingletonComponent::class)
+object TestAppModule {
+
+    @Provides
+    @Named("testDatabase")
+    fun injectInMemoryRoom(@ApplicationContext context: Context) =
+        Room.inMemoryDatabaseBuilder(context, ArtDatabase::class.java)
+            .allowMainThreadQueries()
+            .build()
+}
