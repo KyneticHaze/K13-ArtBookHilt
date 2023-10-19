c7650e3e2a055cc0216b4f44e2deafe3b7e56330
furkan harmancı <rocklee34@hotmail.com>
2023-10-20 00:04:31 +0300

    build: mockito contrib file is added :hammer_and_wrench:

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,12 +42,19 @@ android {
     }
 }
 
+val mockitoVersion = "5.6.0"
+val retrofitVersion = "2.9.0"
+val lifecycleVersion = "2.6.2"
+val roomVersion = "2.6.0"
+val navVersion = "2.7.4"
+
 dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.test.espresso:espresso-contrib:3.5.1")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
@@ -58,25 +65,25 @@ dependencies {
     ksp ("androidx.hilt:hilt-compiler:1.0.0")
 
     // lifecycle-viewModel-liveData
-    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion")
     implementation ("androidx.lifecycle:lifecycle-extensions:2.2.0")
-    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
-    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycleVersion")
+    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleVersion")
+    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleVersion")
     implementation ("androidx.activity:activity-ktx:1.8.0")
 
     // room
-    implementation ("androidx.room:room-runtime:2.5.2")
-    ksp ("androidx.room:room-compiler:2.5.2")
-    implementation ("androidx.room:room-ktx:2.5.2")
+    implementation ("androidx.room:room-runtime:$roomVersion")
+    ksp ("androidx.room:room-compiler:$roomVersion")
+    implementation ("androidx.room:room-ktx:$roomVersion")
     ksp("org.jetbrains.kotlinx:kotlinx-metadata-jvm:0.5.0")
 
     //retrofit2
-    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("com.squareup.retrofit2:retrofit:$retrofitVersion")
+    implementation ("com.squareup.retrofit2:converter-gson:$retrofitVersion")
     //navigation
-    implementation ("androidx.navigation:navigation-fragment-ktx:2.7.4")
-    implementation ("androidx.navigation:navigation-ui-ktx:2.7.4")
+    implementation ("androidx.navigation:navigation-fragment-ktx:$navVersion")
+    implementation ("androidx.navigation:navigation-ui-ktx:$navVersion")
     //glide
     implementation ("com.github.bumptech.glide:glide:4.16.0")
     ksp ("com.github.bumptech.glide:ksp:4.14.2")
@@ -89,16 +96,16 @@ dependencies {
     testImplementation ("org.robolectric:robolectric:4.8.1")
     testImplementation ("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
     testImplementation ("com.google.truth:truth:1.1.4")
-    testImplementation ("org.mockito:mockito-core:4.7.0")
+    testImplementation ("org.mockito:mockito-core:$mockitoVersion")
 
     // Android Test Implementations
     androidTestImplementation ("junit:junit:4.13.2")
-    androidTestImplementation ("org.mockito:mockito-android:5.6.0")
+    androidTestImplementation ("org.mockito:mockito-android:$mockitoVersion")
     androidTestImplementation ("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
     androidTestImplementation ("androidx.arch.core:core-testing:2.2.0")
     androidTestImplementation ("com.google.truth:truth:1.1.4")
     androidTestImplementation ("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation ("org.mockito:mockito-core:4.7.0")
+    androidTestImplementation ("org.mockito:mockito-core:$mockitoVersion")
     androidTestImplementation ("com.google.dagger:hilt-android-testing:2.43.2")
     kspAndroidTest ("com.google.dagger:hilt-android-compiler:2.48.1")
     debugImplementation ("androidx.fragment:fragment-testing:1.7.0-alpha06")
